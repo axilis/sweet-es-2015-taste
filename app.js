6914213544e926ff585eee83a44c06bc9e1fe35b
Damir Sirola <damir.sirola@axilis.com>
2016-03-16 23:56:28 +0100

    var let const

---
+++ app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var answerToLife = 42;
+if (true) {
+  var answerToLife = -1;
+  console.log(answerToLife);
+}
+console.log(answerToLife);
+
+// let answerToLife = 5;
+
+debugger;
+
+fail = "I'll create a GUI interface using Visual Basic, see if I can track an IP Address";
+var fail;
+console.log(fail);
+
+home = "127.0.0.1";
+console.log(home);
+let home;
+console.log(home);
+
+debugger;
+
+if (true) {
+  var value = true;
+}
+console.log(true);
+
+debugger;
+
+if (true) {
+  let lie = false;
+}
+// console.log(lie);
+
+debugger;
+
+let number = 100;
+for (let number = 0; number < 10; number++) {
+  if (number === 9) {
+    console.log('Inside for loop: ' + number);
+  }
+}
+console.log('Outside for loop: ' + number);
+
+debugger;
+
+const maxUsers = 5;
+// maxUsers = 10;
+
+if (true) {
+  const reply = "Make it so";
+  console.log(reply);
+}
+// console.log(reply);
