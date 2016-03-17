43f086070045e21240c08514164907ac2a5bc2b1
Damir Sirola <damir.sirola@axilis.com>
2016-03-17 01:56:45 +0100

    Promises

--- app.js
+++ app.js
@@ -1,66 +1,21 @@
 'use strict';
 
-const RandomCitizen = (function(name) {
-  let _name = name;
-  return {
-    observe: function() {
-      console.log("Admire superhero");
-    },
-    introduce: function() {
-      console.log("I'm " + _name)
-    }
+let peoplePromise = new Promise(
+  function(resolve, reject) {
+    console.log("starting promise at " + performance.now());
+    setTimeout(function() {
+      console.log("promise ended at " + performance.now());
+      let people = [{ id: 1, name: "Xavier" }, { id: 2, name: "Magneto" }];
+      resolve(people);
+    }, Math.random() * 2000 + 2000);
   }
-})
+);
 
-let bruce = new RandomCitizen("Bruce");
-bruce.introduce();
-bruce.observe();
-
-debugger;
-
-function Superhero() {
-  this.fly = function() {
-    console.log("Flying");
-  }
-  this.fight = function() {
-    console.log("Fighting");
-  }
-}
-
-let superman = new Superhero();
-superman.fly();
-
-debugger;
-
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
-  introduce() {
-    console.log("Hi, I'm " + this.name);
-  }
-}
-
-var tony = new Person("tony");
-tony.introduce();
-
-debugger;
-
-class Villan extends Person {
-  constructor(name) {
-    super(name);
-  }
-  fight() {
-    console.log("Fighting dirty");
-  }
-  gloat() {
-    console.log("I'm so much better than you");
-  }
-  introduce() {
-    super.introduce();
-    console.log("Prepare to die (right after I tell you my world domination plan).");
-  }
-}
-
-let ultron = new Villan("Ultron");
-ultron.introduce();
+peoplePromise
+  .then(people => {
+    console.log("Continuing at " + performance.now());
+    console.log(people);
+  })
+  .catch(error => {
+    console.log(error);
+  })
