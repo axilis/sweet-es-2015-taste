ff90d174de599b6ce2415359825d6acdf6174b66
Damir Sirola <damir.sirola@axilis.com>
2016-03-17 01:42:19 +0100

    Classes

--- app.js
+++ app.js
@@ -1,18 +1,66 @@
 'use strict';
 
-import consoleWriter, {red as warning, blue, green}  from './consoleWriter';
+const RandomCitizen = (function(name) {
+  let _name = name;
+  return {
+    observe: function() {
+      console.log("Admire superhero");
+    },
+    introduce: function() {
+      console.log("I'm " + _name)
+    }
+  }
+})
 
-consoleWriter('Plain black text');
+let bruce = new RandomCitizen("Bruce");
+bruce.introduce();
+bruce.observe();
 
-warning("red text");
+debugger;
 
-blue("blue text");
+function Superhero() {
+  this.fly = function() {
+    console.log("Flying");
+  }
+  this.fight = function() {
+    console.log("Fighting");
+  }
+}
 
-import * as writer from './consoleWriter';
+let superman = new Superhero();
+superman.fly();
 
-writer.default("plain");
-writer.red("Colored");
+debugger;
 
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  introduce() {
+    console.log("Hi, I'm " + this.name);
+  }
+}
 
-// green("green text")
-// writer.green("text");
+var tony = new Person("tony");
+tony.introduce();
+
+debugger;
+
+class Villan extends Person {
+  constructor(name) {
+    super(name);
+  }
+  fight() {
+    console.log("Fighting dirty");
+  }
+  gloat() {
+    console.log("I'm so much better than you");
+  }
+  introduce() {
+    super.introduce();
+    console.log("Prepare to die (right after I tell you my world domination plan).");
+  }
+}
+
+let ultron = new Villan("Ultron");
+ultron.introduce();
